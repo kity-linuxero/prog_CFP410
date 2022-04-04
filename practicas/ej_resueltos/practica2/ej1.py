2abdf0280e69835b2f2e728b421df8360eeccc25
Cristian Giambruni <cgiambruni@gmail.com>
2022-04-04 15:52:53 -0300

    Changes Python case

---
+++ practicas/ej_resueltos/practica2/ej1.py
@@ -0,0 +1,15 @@
+letra = 'b'
+
+match letra:
+	case 'a':
+		print ("Vocal")
+	case 'e':
+		print ("Vocal")
+	case 'i':
+		print ("Vocal")
+	case 'o':
+		print ("Vocal")
+	case 'u':
+		print ("Vocal")
+	case _:
+		print ("No es vocal")
