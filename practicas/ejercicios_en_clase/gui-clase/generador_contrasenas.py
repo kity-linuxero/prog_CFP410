0b064846242d93a0dbc4163475fa2feb33c26e94
kity-linuxero <cgiambruni@gmail.com>
2022-10-20 20:19:18 -0300

    fixed errors

--- practicas/ejercicios_en_clase/gui-clase/generador_contrasenas.py
+++ practicas/ejercicios_en_clase/gui-clase/generador_contrasenas.py
@@ -4,12 +4,13 @@ import contras
 
 # Dimensiones de la ventana (en píxeles)
 ANCHO=400
-ALTO=500
+ALTO=150
 
 # Definimos app
 app = tk.Tk()
-app.title("Ejemplo clase")
+app.title("Ejemplo clase generador contraseña")
 app.config(width=ANCHO, height=ALTO)
+app.resizable(0, 0)
 
 # Definimos el texto
 text1 = ttk.Label(text="Longitud de la contraseña: ")
@@ -18,24 +19,30 @@ text1.place(x=20,y=20)
 # Definimos input
 text_input = ttk.Entry()
 #text_input.insert(0,"Escriba su nombre")
-text_input.place(x=160, y=20)
+text_input.place(x=200, y=20)
+
+
+contra = tk.StringVar()
 
 # Función para saludar. Crea un nuevo label
-def saludo():
+def generator():
 
-    
     if len(text_input.get()):
-        cantidad = int(text_input.get())
-        contra = contras.generador_contraseñas(cantidad)
-    #print (text_input.get())
+
+        try:
+            cantidad = int(text_input.get())
+        except:
+            contra.set("ERROR!. Ingrese un número.")
+
+        contra.set(contras.generador_contraseñas(cantidad))
     else:
-        contra = contras.generador_contraseñas(6)
+        contra.set(contras.generador_contraseñas(6))
 
-    text_saludo = ttk.Label(text=contra)
-    text_saludo.place(x=20,y=100)
-
-btn_saludo = ttk.Button(text="Saludame", command=saludo)
+btn_saludo = ttk.Button(text="Generar contraseña", command=generator)
 btn_saludo.place(x=20,y=50)
+
+text_saludo = ttk.Label(app, textvariable = contra)
+text_saludo.place(x=20,y=100)
 
 # Loop de la aplicación
 app.mainloop()
