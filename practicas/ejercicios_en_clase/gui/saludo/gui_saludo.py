dd776d4d113b688920f8c46aee0319fdb9fac782
kity-linuxero <cgiambruni@gmail.com>
2022-10-27 18:40:01 -0300

    Fix bug StringVar

--- practicas/ejercicios_en_clase/gui/saludo/gui_saludo.py
+++ practicas/ejercicios_en_clase/gui/saludo/gui_saludo.py
@@ -19,14 +19,23 @@ text_input = ttk.Entry()
 #text_input.insert(0,"Escriba su nombre")
 text_input.place(x=160, y=20)
 
+# Definimos el texto del saludo
+str_saludo = tk.StringVar()
+ttk.Label()
+
+# Observar el textvariable y se setea la variable que el label estará observando.
+# Si la variable cambia, el label cambiará automáticamente
+text_saludo = ttk.Label(app, textvariable = str_saludo)
+# Ubicación de la contraseña
+text_saludo.place(x=20,y=110)
+
 # Función para saludar. Crea un nuevo label
 def saludo():
     if len(text_input.get()):
-        text_saludo = ttk.Label(text="Hola " + text_input.get())
-    #print (text_input.get())
+        str_saludo.set(f"Hola {text_input.get()}")
     else:
-        text_saludo = ttk.Label(text="Hola amigazo")
-    text_saludo.place(x=20,y=100)
+        str_saludo.set(f"Hola amigazo!")
+
 
 btn_saludo = ttk.Button(text="Saludame", command=saludo)
 btn_saludo.place(x=20,y=50)
